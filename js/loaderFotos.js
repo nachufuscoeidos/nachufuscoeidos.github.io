6a2d2547c01e940ba0273e5a6df4f7a6e1a0fe25
nachufuscoeidos <nadia@eidosglobal.org>
2020-05-16 20:17:59 -0300

    test fotos automaticas 2

--- js/loaderFotos.js
+++ js/loaderFotos.js
@@ -12,9 +12,40 @@ function loaderSlider(fuente, containerUl) {
             fotos.push(archivos[i].trim());
         }
 
-        console.log(fotos);
-        //fetch("https://raw.githubusercontent.com/nachufuscoeidos/recursos_abrazo/master/" + fuente)
+        fetch("https://raw.githubusercontent.com/nachufuscoeidos/recursos_abrazo/master/" + fuente)
+        .then(function(response) {
+            return response.text();
+        })
+        .then(function(archivosStringGithub) {
+            let archivosGithub = archivosStringGithub.split(",");
+
+            for (let i = 0; i < archivosGithub.length; i++) {
+                if (!fotos.includes(archivosGithub[i])) {
+                    fotos.push(archivosGithub[i].trim());
+                }
+            }
+
+            fillUlSlider(fotos, containerUl);
+        })
+        .catch(function(e) {
+            console.log("Error accesing file in Github: " + fuente);
+            console.log(e);
+            
+            fillUlSlider(fotos, containerUl);
+        })
     })
+}
+
+function fillUlSlider(fotos, containerUl) {
+    let ul = document.querySelector(containerUl);
+
+    for (let i = 0; i < fotos.length; i++) {
+        let li = "<li>"
+        li += "<img src='" + fotos[i] + "'>"
+        li += "</li>"
+
+        ul.innerHTML += li;
+    }
 }
 
 function loaderSlideshow(fuentes, containerUl) {
