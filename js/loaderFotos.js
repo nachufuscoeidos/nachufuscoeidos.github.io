d838640e68d6184f8db5bad2e9aa1ecda4e9ed81
nachufuscoeidos <nadia@eidosglobal.org>
2020-05-16 20:35:46 -0300

    Simplificando y sin pegarle a github

--- js/loaderFotos.js
+++ js/loaderFotos.js
@@ -12,31 +12,7 @@ function loaderSlider(fuente, containerUl) {
             fotos.push(archivos[i].trim());
         }
 
-        fetch("https://raw.githubusercontent.com/nachufuscoeidos/recursos_abrazo/master/" + fuente)
-        .then(function(response) {
-            if (!response.ok) {
-                // make the promise be rejected if we didn't get a 2xx response
-                throw new Error("Not 2xx response")
-            }
-            return response.text();
-        })
-        .then(function(archivosStringGithub) {
-            let archivosGithub = archivosStringGithub.split(",");
-
-            for (let i = 0; i < archivosGithub.length; i++) {
-                if (!fotos.includes(archivosGithub[i])) {
-                    fotos.push(archivosGithub[i].trim());
-                }
-            }
-
-            fillUlSlider(fotos, containerUl);
-        })
-        .catch(function(e) {
-            console.log("Error accesing file in Github: " + fuente);
-            console.log(e);
-            
-            fillUlSlider(fotos, containerUl);
-        })
+        fillUlSlider(fotos, containerUl);
     })
 }
 
